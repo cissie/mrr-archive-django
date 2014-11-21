28a183b738254ccc19ea5ad2299a2d63cf7af54e
student <cissie@gmail.comgit statuslsqgit config --global user.email cissie@gmail.com>
2014-11-20 21:25:52 -0800

    minor advances

--- library/models.py
+++ library/models.py
@@ -3,48 +3,41 @@ from django.db import models
 
 class Artist(models.Model):
     artist = models.CharField(max_length=200)
-    column1 = models.Field.db_column
 
 
 class RecordTitle(models.Model):
     artist = models.ForeignKey(Artist)
     title = models.CharField(max_length=200)
-    column2 = models.Field.db_column
     
 
 class Format(models.Model):
     record_title = models.ForeignKey(RecordTitle)
     format = models.CharField(max_length=200)
-    column3 = models.Field.db_column
 
 
 class Country(models.Model):
     artist = models.ForeignKey(Artist)
     country = models.CharField(max_length=200)
-    column7 = models.Field.db_column
 
 
 class Year(models.Model):
     record_title = models.ForeignKey(RecordTitle)
     year = models.CharField(max_length=200)
-    column6 = models.Field.db_column
 
 
-class CoverArt(models.Model):
-    record_title = models.ForeignKey(RecordTitle)
-    cover_art = models.ImageField(height_field=None, width_field=None, max_length=200)
+# class CoverArt(models.Model):
+#     record_title = models.ForeignKey(RecordTitle)
+#     cover_art = models.ImageField(height_field=None, width_field=None, max_length=200)
 
 
 class Label(models.Model):
     record_title = models.ForeignKey(RecordTitle)
     label = models.CharField(max_length=200)
-    column4 = models.Field.db_column
 
 
 class CatalogNumber(models.Model):
     label = models.ForeignKey(Label)
     catalog_number = models.CharField(max_length=200)
-    column5 = models.Field.db_column
 
 
 class Discography(models.Model):
@@ -59,7 +52,7 @@ class Members(models.Model):
 
 class Review(models.Model):
     record_title = models.ForeignKey(RecordTitle)
-    review = models.TextField
+    review = models.TextField()
 
 
 class IssueNumber(models.Model):
---
+++ library/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import patterns, url
+from library import views
+
+urlpatterns = patterns('',
+        url(r'^$', views.index, name='index'))
