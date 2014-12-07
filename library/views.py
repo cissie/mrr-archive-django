87f783582677760aace5a601eb269bef989340a5
cissie <cissie@gmail.com>
2014-12-06 18:45:21 -0800

    updated views and dom.html

--- library/views.py
+++ library/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_exempt
+from json import dumps, loads
+from library.forms import ArtistForm
 from library.models import Artist
 from library.models import RecordTitle
-from library.forms import ArtistForm
-from json import dumps
 
 
 def index(request):
@@ -27,10 +28,12 @@ def index(request):
     # Render the response and send it back!
     return render_to_response('library/index.html', context_dict, context)
 
-
+@csrf_exempt
 def ajax(request):
-    # Obtain the context from the HTTP request.
-    context = RequestContext(request)
+    if request.method == "POST":
+        new_artist = Artist()
+        new_artist.name = request.POST["artist"]
+        new_artist.save()
 
     # Query the database for a list of ALL artists currently stored.
     # Place the list in our context_dict dictionary which will be passed to the template engine.
