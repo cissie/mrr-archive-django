7e9d70ef81d7283ff3294ac0c1dc096ec37e708a
cissie <cissie@gmail.com>
2014-12-18 21:24:41 -0800

    rewrote the views and template for login and registration

--- library/views.py
+++ library/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.shortcuts import redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render, render_to_response
@@ -8,6 +11,7 @@ from json import dumps, loads
 from library.forms import ArtistForm, UserForm, UserProfileForm
 from library.models import Artist, RecordTitle, Country, FormatType, ReleaseYear, RecordLabel, RecordReview, \
     CatalogNumber, IssueNumber, FileUnder, Notes
+from time import sleep
 import json
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "library.settings")
@@ -57,24 +61,24 @@ def dom(request):
     return render(request, 'library/dom.html')
 
 
-def artist(request, artist_name_url):
+def artist(request, artist_id):
     # Request our context from the request passed to us.
     context = RequestContext(request)
 
     # Change underscores in the artist name to spaces.
     # URLs don't handle spaces well, so we encode them as underscores.
     # We can then simply replace the underscores with spaces again to get the name.
-    artist_name = artist_name_url.replace('_', ' ')
+    artist_id = artist_id.replace('_', ' ')
 
     # Create a context dictionary which we can pass to the template rendering engine.
     # We start by containing the name of the artist passed by the user.
-    context_dict = {'artist_name': artist_name}
+    context_dict = {'artist_id': artist_id}
 
     try:
         # Can we find an artist with the given name?
         # If we can't, the .get() method raises a DoesNotExist exception.
         # So the .get() method returns one model instance or raises an exception.
-        artist = Artist.objects.get(artist=artist_name)
+        artist = Artist.objects.get(id=artist_id)
 
         # Retrieve all of the associated titles.
         # Note that filter returns >= 1 model instance.
@@ -137,104 +141,26 @@ def add_artist(request):
 
 
 def register(request):
-    # Like before, get the request's context.
-    context = RequestContext(request)
+    if request.method == "POST":
+        User.objects.create_user(request.POST["username"], None, request.POST["password"])
+    return render(request, 'library/register.html')
 
-    # A boolean value for telling the template whether the registration was successful.
-    # Set to False initially. Code changes value to True when registration succeeds.
-    registered = False
 
-    # If it's a HTTP POST, we're interested in processing form data.
-    if request.method == 'POST':
-        # Attempt to grab information from the raw form information.
-        # Note that we make use of both UserForm and UserProfileForm.
-        user_form = UserForm(data=request.POST)
-        profile_form = UserProfileForm(data=request.POST)
-
-        # If the two forms are valid...
-        if user_form.is_valid() and profile_form.is_valid():
-            # Save the user's form data to the database.
-            user = user_form.save()
-
-            # Now we hash the password with the set_password method.
-            # Once hashed, we can update the user object.
-            user.set_password(user.password)
-            user.save()
-
-            # Now sort out the UserProfile instance.
-            # Since we need to set the user attribute ourselves, we set commit=False.
-            # This delays saving the model until we're ready to avoid integrity problems.
-            profile = profile_form.save(commit=False)
-            profile.user = user
-
-            # Did the user provide a profile picture?
-            # If so, we need to get it from the input form and put it in the UserProfile model.
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
-
-            # Now we save the UserProfile model instance.
-            profile.save()
-
-            # Update our variable to tell the template registration was successful.
-            registered = True
-
-        # Invalid form or forms - mistakes or something else?
-        # Print problems to the terminal.
-        # They'll also be shown to the user.
-        else:
-            print user_form.errors, profile_form.errors
-
-    # Not a HTTP POST, so we render our form using two ModelForm instances.
-    # These forms will be blank, ready for user input.
-    else:
-        user_form = UserForm()
-        profile_form = UserProfileForm()
-
-    # Render the template depending on the context.
-    return render_to_response(
-            'library/register.html',
-            {'user_form': user_form, 'profile_form': profile_form, 'registered': registered},
-            context)
-
-def user_login(request):
-    # Like before, obtain the context for the user's request.
-    context = RequestContext(request)
-
-    # If the request is a HTTP POST, try to pull out the relevant information.
-    if request.method == 'POST':
-        # Gather the username and password provided by the user.
-        # This information is obtained from the login form.
-        username = request.POST['username']
-        password = request.POST['password']
-
-        # Use Django's machinery to attempt to see if the username/password
-        # combination is valid - a User object is returned if it is.
-        user = authenticate(username=username, password=password)
-
-        # If we have a User object, the details are correct.
-        # If None (Python's way of representing the absence of a value), no user
-        # with matching credentials was found.
-        if user:
-            # Is the account active? It could have been disabled.
+def login(request):
+    if request.method == "POST":
+        user =auth.authenticate(username=request.POST["username"],
+                                password=request.POST["password"])
+        if user is not None:
+            # the password verified for the user
             if user.is_active:
-                # If the account is valid and active, we can log the user in.
-                # We'll send the user back to the homepage.
-                login(request, user)
-                return HttpResponseRedirect('/library/')
+                print("User is valid, active and authenticated")
+                return redirect('index')
             else:
-                # An inactive account was used - no logging in!
-                return HttpResponse("Your MRR Archive account is disabled.")
-        else:
-            # Bad login details were provided. So we can't log the user in.
-            print "Invalid login details: {0}, {1}".format(username, password)
-            return HttpResponse("Invalid login details supplied.")
+                print("The password is valid, but the account has been disabled!")
 
-    # The request is not a HTTP POST, so display the login form.
-    # This scenario would most likely be a HTTP GET.
-    else:
-        # No context variables to pass to the template system, hence the
-        # blank dictionary object...
-        return render_to_response('library/login.html', {}, context)
+        else:
+            print("The username and password were incorrect.")
+    return render(request, 'library/login.html')
 
 @login_required
 def restricted(request):
@@ -255,15 +181,28 @@ def load_data(request):
     with open("record_collection.json") as f:
         json_data = json.load(f)
         last_artist = ""
+        count = 0
         for d in json_data:
             if last_artist != d["artist"]:
-                print d["artist"]
                 new_artist = Artist()
                 new_artist.name = d["artist"]
                 new_artist.save()
+                # artist_id = new_artist.id
+                sleep(0.05)
                 last_artist = d["artist"]
+                print "adding" + d["artist"]
+            else:
+                print "skipping"
+            count += 1
+            print count
             #start adding records at same indentation as above
-
+        # for d in json_data:
+        #     print("else")
+        #     new_title = RecordTitle()
+        #     new_title.record_title = d["record_title"]
+        #     new_title.artist = Artist.objects.get(id=artist_id)
+        #     new_title.save()
+        #     sleep(0.05)
             # for k, v in d.iteritems():
                 # print k, v
         # artist = Artist()
