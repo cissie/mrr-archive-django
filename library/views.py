27f2d2611f732463a4fbbc0bdb16af35a75e660f
cissie <cissie@gmail.com>
2015-03-07 11:34:51 -0800

    load_data script has quit working on AWS instance, made another change

--- library/views.py
+++ library/views.py
@@ -617,13 +617,11 @@ def load_data(request):
                 format_type.save()
             new_title.format_type = format_type
                # allows the same release year for multiple titles/artists
-            if d["release_year"] is not None:
-                try:
-                    release_year = ReleaseYear.objects.filter(release_year=d["release_year"])
-                except ReleaseYear.DoesNotExist:
-                    release_year = ReleaseYear(release_year=d["release_year"])
-                    release_year.save()
-                new_title.release_year = release_year
+            try:
+                release_year = ReleaseYear.objects.filter(release_year=d["release_year"])
+            except ReleaseYear.DoesNotExist:
+                release_year = ReleaseYear(release_year=d["release_year"])
+                release_year.save()
             record_labels = RecordLabel.objects.filter(record_label=d['label_name'])
             record_labels = list(record_labels)
             if not len(record_labels) == 0:
