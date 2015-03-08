cb66477bda73bfab9c63a1de7449a17aa420bec2
cissie <cissie@gmail.com>
2015-03-08 09:55:12 -0700

    Maybe this will work

--- library/views.py
+++ library/views.py
@@ -619,7 +619,7 @@ def load_data(request):
                # allows the same release year for multiple titles/artists
             if d["release_year"] is not None:
                 try:
-                    release_year = ReleaseYear.objects.get(release_year=d["release_year"])
+                    release_year = ReleaseYear.objects.filter(release_year=d["release_year"])
                 except ReleaseYear.DoesNotExist:
                     release_year = ReleaseYear(release_year=d["release_year"])
                     release_year.save()
